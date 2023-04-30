af4e26632f04f5c87636176604d3be73e72bdf55
Rohan Poreddy <rohanporeddy@Rohans-MacBook-Pro.local>
2023-04-29 19:30:09 -0600

    new changes been updated

--- app.py
+++ app.py
@@ -10,26 +10,17 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        user_value = request.form["user_value"]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "assistant", "content": user_value},
+                
+            ]
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+
+        return redirect(url_for("index", result=response.choices[0].message.content))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
